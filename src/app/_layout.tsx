0fb0287aa0e3052cab5fe3806d663e5918604b09
Aran Leite <hyoretsu@gmail.com>
2023-07-21 15:05:41 -0300

    Add fonts to the app
    More specifically, Inter 500

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,22 @@
+import { Inter_500Medium } from "@expo-google-fonts/inter";
+import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+SplashScreen.preventAutoHideAsync();
+
 const Layout: React.FC = () => {
+	const [fontsLoaded] = useFonts({
+		Inter_500Medium,
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	} else if (fontsLoaded) {
+		SplashScreen.hideAsync();
+	}
+
 	return (
 		<SafeAreaView>
 			<Slot />
