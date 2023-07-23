8acc8c7a001277d498441fd84b43909d4862d0e2
Aran Leite <hyoretsu@gmail.com>
2023-07-23 15:31:10 -0300

    Static method list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,10 @@ import { Text, View } from "react-native";
 
 import { vh, vw } from "@units";
 
-const methodCategories = ["Autorais", "Integração", "Interpolação", "Matrizes", "Zeros de função"];
+const methodCategories = ["Zeros de função", "Interpolação", "Integração", "Matrizes", "Autorais"];
+const methodTitles: Record<string, string[]> = {
+	"Zeros de função": ["Bisseção", "Falsa posição", "Newton-Raphson", "Secante"],
+};
 
 const Home: React.FC = () => {
 	return (
@@ -18,16 +21,45 @@ const Home: React.FC = () => {
 					style={{
 						borderRadius: 4,
 						borderWidth: 1,
-						flexDirection: "row",
-						justifyContent: "space-between",
 						margin: 2 * vh,
 						marginBottom: 0,
-						paddingHorizontal: 7 * vw,
-						paddingVertical: 2.35 * vh,
 					}}
 				>
-					<Text style={{ color: "#4E4E4E", fontFamily: "Inter_500Medium", fontSize: 19 }}>{category}</Text>
-					<Feather name="chevron-down" size={26} color="black" />
+					<View
+						style={{
+							flexDirection: "row",
+							justifyContent: "space-between",
+							paddingHorizontal: 7 * vw,
+							paddingVertical: 2.35 * vh,
+						}}
+					>
+						<Text style={{ color: "#4E4E4E", fontFamily: "Inter_500Medium", fontSize: 19 }}>{category}</Text>
+						<Feather name="chevron-down" size={26} color="black" />
+					</View>
+
+					<View
+						style={{
+							borderLeftColor: "#4B00D1",
+							borderLeftWidth: 4,
+						}}
+					>
+						{methodTitles[category]?.map(title => (
+							<Text
+								key={title}
+								style={{
+									borderTopColor: "#D9D9D9",
+									borderTopWidth: 2,
+									color: "#4E4E4E",
+									fontFamily: "Inter_500Medium",
+									fontSize: 19,
+									paddingHorizontal: 7 * vw,
+									paddingVertical: 2.35 * vh,
+								}}
+							>
+								{title}
+							</Text>
+						))}
+					</View>
 				</View>
 			))}
 		</>
